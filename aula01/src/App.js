e2712d5b3e6f4c064c00116db33dce4f6a3b43f4
SamuelThales <95038076+Samoo2@users.noreply.github.com>
2023-04-05 11:58:57 -0300

    Add files via upload

---
+++ aula01/src/App.js
@@ -0,0 +1,14 @@
+
+import Relogio from "./Components/Relogio";
+import Diversos from "./Components/Diversos";
+
+function App() {
+  return (
+    <>
+      <Diversos />
+      <Relogio />
+    </>
+  );
+}
+
+export default App;
