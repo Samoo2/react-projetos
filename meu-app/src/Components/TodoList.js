a6b75bd54e033b784d2ac56a4399456a5175de2f
SamuelThales <95038076+Samoo2@users.noreply.github.com>
2023-05-03 12:00:28 -0300

    Add files via upload

---
+++ meu-app/src/Components/TodoList.js
@@ -0,0 +1,18 @@
+export default function TodoList() {
+    return (
+        <>
+            <h3> Hedy Lamarr´s Todos</h3>
+            <img 
+                src="https://i.imgur.com/yX0vdSs.jpg"
+                alt="Hedy Lamarr"
+                class="photo"
+                />
+            <ul>
+                <li Invent new traffic lights />
+                <li Rehearse a movie scene />
+                <li Improve the spectrum technology />
+            </ul>
+        </>
+    )
+    
+}
