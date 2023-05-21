c8e7f08532e10e51cbf1b0c4ad03ed2cc19dd628
SamuelThales <95038076+Samoo2@users.noreply.github.com>
2023-05-21 20:36:07 -0300

    Update App.js

--- meu-app/src/App.js
+++ meu-app/src/App.js
@@ -7,6 +7,14 @@ import Avatar from "./Components/Avatar";
 import TodoList from "./Components/TodoList";
 import Toolbar from "./Components/Toolbar";
 import Gallerys from "./Components/Gallerys";
+import Counter from "./Components/Counter";
+import Form from "./Components/Form";
+import Form2 from "./Components/Form2";
+import Form3 from "./Components/Form3";
+import List  from "./Components/List";
+import List2  from "./Components/List2";
+import MovingDot  from "./Components/MovingDot";
+import ShapeEditor  from "./Components/ShapeEditor";
 
 function App() {
   return (
@@ -26,6 +34,15 @@ function App() {
        <h2> Aula 04</h2>
         <Toolbar />
         <Gallerys />
+        <Counter />
+        <List />
+        <List2 />
+        <Form />
+        <Form2 />
+        <Form3 />
+        <MovingDot />
+        <ShapeEditor />
+
     </div>
   );
 }
