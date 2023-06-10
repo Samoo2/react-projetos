82bee68b53c7c4c05dc6a91188e4b04906f13c70
SamuelThales <95038076+Samoo2@users.noreply.github.com>
2023-06-10 15:28:28 -0300

    Update Atv02.js

--- react-app/src/Atv02.js
+++ react-app/src/Atv02.js
@@ -1,9 +1,11 @@
 import ContadorPessoas from "./Components/ContadorPessoas";
+import { Link } from "react-router-dom";
 
-export default function Atv01(){
+export default function Atv02(){
     return (
         <>
           <h2> Atividade 02</h2>
+          <Link className="voltar" to="/"> Voltar</Link>
           <ContadorPessoas />
         </>
     );
